37c8290e59ab754b7f0bb8d8c66f5e8432b2461c
Peter Nerg <peter.nerg@gmail.com>
2021-09-09 07:30:30 +0200

    added test yaml for 'app'

--- app/src/test/java/org/dmonix/area51/config/ConfigurationObjectTest.java
+++ app/src/test/java/org/dmonix/area51/config/ConfigurationObjectTest.java
@@ -6,27 +6,32 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest // needed or else the application.yaml is not read
 @EnableConfigurationProperties(
     value = ConfigurationObject.class) // must be set or we get FUBAR exception at startup
+@ActiveProfiles("test")
 public class ConfigurationObjectTest {
   @Autowired ConfigurationObject config;
 
   @Test
   public void assertHost() {
+    // this value is from application.yaml
     Assert.assertEquals("127.0.0.1", config.host);
   }
 
   @Test
   public void assertPort() {
+    // this value is from application.yaml
     Assert.assertEquals(8080, config.port);
   }
 
   @Test
   public void assertInterval() {
-    Assert.assertEquals(69, config.interval.toSeconds());
+    // this value is from application-test.yaml
+    Assert.assertEquals(96, config.interval.toSeconds());
   }
 }
